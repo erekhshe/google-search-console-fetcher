cd96f5f52932d0e16e823bbe06f75b3433b6bef3
Arash Ebrahimi <erekhshe@gmail.com>
2024-05-15 15:33:54 +0330

    Create AppTest.java

---
+++ src/test/java/test/gscfetcher/AppTest.java
@@ -0,0 +1,21 @@
+package test.gscfetcher;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import arash.gscfetcher.GSCFetcher;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	GSCFetcher.MyGSCFetcherFunction();
+    	assertTrue( true );
+        
+    }
+}
