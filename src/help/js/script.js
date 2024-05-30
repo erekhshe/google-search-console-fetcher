9f5acf72284142af5b1d871e6cd3987b5a5e11d7
Arash Ebrahimi <erekhshe@gmail.com>
2024-05-30 08:29:47 +0330

    Update script.js

--- src/help/js/script.js
+++ src/help/js/script.js
@@ -1,1 +1,11 @@
+function openModal(img) {
+  var modal = document.getElementById("modal");
+  var modalImg = document.getElementById("modal-image");
+  modal.style.display = "block";
+  modalImg.src = img.src;
+}
 
+function closeModal() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
